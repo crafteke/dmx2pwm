cd1a8b494dd9a7012b7278833a3d884625dbe37b
Pierre-Henri Wibaut <wibautph@gmail.com>
2021-05-28 09:07:22 +0000

    lightDMXController.py edited online with Bitbucket

--- lightDMXController.py
+++ lightDMXController.py
@@ -6,30 +6,31 @@ from ola.ClientWrapper import ClientWrapper
 
 GPIO.setmode(GPIO.BCM)
 
-NUMBER_OUTPUT=28
 PWM_FREQ=100
 REFRESH_DELAY=int(1000/30)
-
-pwm_pins= [None]*NUMBER_OUTPUT
-pwm_values =[0]*NUMBER_OUTPUT
+#default: all pins active as PWM output
+enabled_outputs = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27]
+pwm_pins= [None]*len(enabled_outputs)
+pwm_values =[0]*len(enabled_outputs)
 
 def initPWM():
-    for x in range(0,NUMBER_OUTPUT):
-        GPIO.setup(x, GPIO.OUT)
-        pwm_pins[x]=GPIO.PWM(x,PWM_FREQ)
+    #for x in range(0,NUMBER_OUTPUT):
+    for x in range(0,len(enabled_outputs)):
+        GPIO.setup(enabled_outputs[x], GPIO.OUT)
+        pwm_pins[x]=GPIO.PWM(enabled_outputs[x],PWM_FREQ)
         pwm_pins[x].start(0)
 
 def stopPWM():
-    for x in range(0,NUMBER_OUTPUT):
+    for x in range(0,len(enabled_outputs)):
         pwm_pins[x].stop()
 
 def updatePWM():
     wrapper.AddEvent(REFRESH_DELAY, updatePWM)
-    for x in range(0,NUMBER_OUTPUT):
+    for x in range(0,len(enabled_outputs)):
         pwm_pins[x].ChangeDutyCycle(int(pwm_values[x]*100/255))
 
 def NewData(data):
-    for x in range(0,NUMBER_OUTPUT):
+    for x in range(0,len(enabled_outputs)):
         pwm_values[x]=data[x]
 
 def handler(signal_received, frame):
@@ -43,10 +44,21 @@ def handler(signal_received, frame):
 
 if __name__ == "__main__":
     signal(SIGINT, handler)
+    config_file=open("/boot/dmx.config",'r')
+    s=config_file.read()
+    params = list(map(lambda e : e.split('\t') ,s.split('\n')))
+    name=list(filter(lambda e: e[0]=='name',params))[0][1]
+    pins=list(filter(lambda e: e[0]=='pins',params))[0][1].split(',')
+    universe=int(list(filter(lambda e: e[0]=='universe',params))[0][1])
+    enabled_outputs = list(map(int,pins))
+    pwm_pins= [None]*len(enabled_outputs)
+    pwm_values =[0]*len(enabled_outputs)
+    enabled_outputs=list(map(int,pins))
+    print("Starting DMX2PWM. ",len(enabled_outputs)," outputs activated. Listening on universe ",universe,".")
     # Clear all the pixels to turn them off.
     wrapper = ClientWrapper()
     initPWM()
     client = wrapper.Client()
-    client.RegisterUniverse(3, client.REGISTER, NewData)
+    client.RegisterUniverse(universe, client.REGISTER, NewData)
     wrapper.AddEvent(REFRESH_DELAY, updatePWM)
     wrapper.Run()
